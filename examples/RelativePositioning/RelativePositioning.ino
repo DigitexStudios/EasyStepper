79ae5553dabfdfa6c4e5ea13a0bde3a26961f305
DigitexStudios <62073336+DigitexStudios@users.noreply.github.com>
2023-01-01 20:31:26 -0500

    Update RelativePositioning.ino

--- examples/RelativePositioning/RelativePositioning.ino
+++ examples/RelativePositioning/RelativePositioning.ino
@@ -4,9 +4,9 @@
  * Created by Liam Timpane
  */
 
-#include "StepServo.h" // Include the stepper servo library
+#include "EasyStepper.h" // Include the stepper servo library
 
-StepServo motor(3, 2, 4); // Create the motor object. The stepper driver has the STP pin on pin 3, DIR pin on pin 2, and ENABLE pin on pin 4.
+EasyStepper motor(3, 2, 4); // Create the motor object. The stepper driver has the STP pin on pin 3, DIR pin on pin 2, and ENABLE pin on pin 4.
 
 void setup() {
   
